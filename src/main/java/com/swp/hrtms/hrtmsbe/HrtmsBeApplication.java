555ef418279b7b74cf75dc4ca77df84659f220f3
AnVu05 <vudinhan2k5@gmail.com>
2026-06-12 20:58:19 +0700

    first commit

---
+++ src/main/java/com/swp/hrtms/hrtmsbe/HrtmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.swp.hrtms.hrtmsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrtmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrtmsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/hrtms/hrtmsbe/HrtmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.hrtms.hrtmsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrtmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
